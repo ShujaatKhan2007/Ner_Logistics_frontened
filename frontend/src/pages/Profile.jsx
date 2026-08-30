0153b5fb04faacdab5f191b94cab6b4f58bddbe1
ShujaatKhan2007 <shujaatkhan2007@gmail.com>
2026-08-30 11:39:53 +0530

    Delete frontend directory

--- frontend/src/pages/Profile.jsx
+++
@@ -1,7 +0,0 @@
-import PageShell from '../components/PageShell.jsx';
-import desktopHtml from '../fragments/desktop-profile.html?raw';
-import mobileHtml from '../fragments/mobile-profile.html?raw';
-
-export default function Profile() {
-  return <PageShell desktopHtml={desktopHtml} mobileHtml={mobileHtml} />;
-}
--- frontend/src/pages/ReportIncident.jsx
+++
@@ -1,7 +0,0 @@
-import PageShell from '../components/PageShell.jsx';
-import desktopHtml from '../fragments/desktop-report.html?raw';
-import mobileHtml from '../fragments/mobile-report.html?raw';
-
-export default function ReportIncident() {
-  return <PageShell desktopHtml={desktopHtml} mobileHtml={mobileHtml} />;
-}
