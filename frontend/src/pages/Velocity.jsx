2278c3db8e43dce68047a56bca9d978f9bfcc7af
ShujaatKhan2007 <shujaatkhan2007@gmail.com>
2026-08-31 00:29:36 +0530

    Delete frontend directory

--- frontend/src/pages/Velocity.jsx
+++
@@ -1,7 +0,0 @@
-import PageShell from '../components/PageShell.jsx';
-import desktopHtml from '../fragments/desktop-velocity.html?raw';
-import mobileHtml from '../fragments/mobile-velocity.html?raw';
-
-export default function Velocity() {
-  return <PageShell desktopHtml={desktopHtml} mobileHtml={mobileHtml} />;
-}
